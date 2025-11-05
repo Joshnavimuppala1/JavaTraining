05f1bf8469c1fa78c17a82955682e411d15c2f63
Joshnavi <joshnavimuppala@gmail.com>
2025-11-05 22:39:12 +0530

    Lambda

---
+++ src/Program2.java
@@ -0,0 +1,14 @@
+
+//SUM OF ALL ELEMENTS
+
+public class Program2 {
+    public static void main(String[]args){
+      
+        int [] array={5,10,15,20};
+        int sum = 0;
+        for(int i =0;i<array.length;i++){
+              sum += array[i];
+            System.out.println("Sum: " + sum);
+        }
+    }
+}
---
+++ src/Program3.java
@@ -0,0 +1,19 @@
+public class Program3 {
+    public static void main(String[] args){
+        int[] arr = {3, 8, 1, 6, 5};
+        int key = 6;
+        boolean found = false;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == key) {
+                found = true;
+                System.out.println("Found at index " + i);
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Not found");
+        }
+    }
+}
